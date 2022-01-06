0b801ca7fa768db96fb9fa2518c9b48957411363
Daniel <danlpsarcanjo@gmail.com>
2022-01-05 19:53:48 -0500

    Setup TypeScript, Eslint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'prettier/prettier': ['warn', {
+      'singleQuote': true,
+      'trailingComma': 'all',
+      'endOfLine': 'auto',
+      'printWidth': 100
+    }],
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/ban-types': 'warn',
+  },
+};
