c7bdc27d2cbc1399de7dfdcd0bfe4fec4547940d
Daniel <danlpsarcanjo@gmail.com>
2022-01-07 09:56:39 -0500

    Refactor check functions

--- src/dijkstra.ts
+++ src/dijkstra.ts
@@ -1,23 +1,10 @@
 export const dijkstra = (graph: number[][], source: number, target: number): number[] | null => {
   const vertexCount = graph.length;
 
-  const isSquareMatrix = graph.every((row) => row.length === vertexCount);
-
-  if (!isSquareMatrix) {
-    throw new Error('Graph should be a square matrix');
-  }
-
-  if (source < 0 || source >= vertexCount) {
-    throw new Error('Source is out of bounds');
-  }
-
-  if (target < 0 || target >= vertexCount) {
-    throw new Error('Target is out of bounds');
-  }
-
-  if (source === target) {
-    throw new Error('Source and target must be different');
-  }
+  checkSquareMatrix(graph, vertexCount);
+  checkSourceInBounds(source, vertexCount);
+  checkTargetInBounds(target, vertexCount);
+  checkSourceAndTargetDifferent(source, target);
 
   const costs = [...Array(vertexCount)].map(() => Infinity);
   costs[source] = 0;
@@ -65,6 +52,32 @@ export const dijkstra = (graph: number[][], source: number, target: number): num
 
   return path;
 };
+
+function checkSourceAndTargetDifferent(source: number, target: number): void {
+  if (source === target) {
+    throw new Error('Source and target must be different');
+  }
+}
+
+function checkTargetInBounds(target: number, vertexCount: number): void {
+  if (target < 0 || target >= vertexCount) {
+    throw new Error('Target is out of bounds');
+  }
+}
+
+function checkSourceInBounds(source: number, vertexCount: number): void {
+  if (source < 0 || source >= vertexCount) {
+    throw new Error('Source is out of bounds');
+  }
+}
+
+function checkSquareMatrix(graph: number[][], vertexCount: number): void {
+  const isSquareMatrix = graph.every((row) => row.length === vertexCount);
+
+  if (!isSquareMatrix) {
+    throw new Error('Graph should be a square matrix');
+  }
+}
 
 function isSmallestCost(vertex: number, vertices: number[], costs: number[]): boolean {
   if (costs[vertex] === Infinity) {
